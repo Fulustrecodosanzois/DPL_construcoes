e01c5fad1e7daf5d6f9b675fa774148860a39f75
Luciano Silva <luciano_jet@hotmail.com>
2023-09-22 00:37:41 -0300

    ajustado camera

--- js/camera.js
+++ js/camera.js
@@ -1,31 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Selecione o botão de captura e o input de arquivo
-    const captureButton = document.getElementById("captureButton");
-    const cameraInput = document.getElementById("cameraInput");
+
+function handleFileSelect(evt) {
+    const file = evt.target.files[0]; 
+    const reader = new FileReader();
   
-    // Adicione um ouvinte de evento de clique ao botão de captura
-    captureButton.addEventListener("click", function () {
-      // Verifique se o navegador suporta a API getUserMedia
-      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        // Solicite permissão para acessar a câmera
-        navigator.mediaDevices
-          .getUserMedia({ video: true }) // Aqui você pode especificar as opções da câmera (por exemplo, front ou back)
-          .then(function (stream) {
-            // A câmera está disponível, exiba o stream de vídeo em uma imagem
-            const photo = document.getElementById("photo");
-            photo.style.display = "block";
-            photo.srcObject = stream;
+    reader.onload = function (e) {
+      
+      const imgElement = document.getElementById('photo');
+      imgElement.src = e.target.result; 
+      imgElement.style.display = 'block'; 
+    };
   
-            // Oculte o botão de captura e o input de arquivo
-            captureButton.style.display = "none";
-            cameraInput.style.display = "none";
-          })
-          .catch(function (error) {
-            console.error("Erro ao acessar a câmera: " + error);
-          });
-      } else {
-        console.error("Seu navegador não suporta a API getUserMedia.");
-      }
-    });
+       reader.readAsDataURL(file);
+  }
+  
+
+  document.getElementById('captureButton').addEventListener('click', function () {
+  
+    const fileInput = document.createElement('input');
+    fileInput.type = 'file';
+    fileInput.accept = 'image/*'; 
+  
+    fileInput.addEventListener('change', handleFileSelect);
+
+    fileInput.click();
   });
   
