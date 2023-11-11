a2fae81936e444e1242289c5eafe76b546a45b08
Werneck <werneckcolins@outlook.com>
2023-11-11 14:37:18 -0300

    Projeto nos conformes.

--- js/enviar.js
+++ js/enviar.js
@@ -1,38 +1,35 @@
-// let relatorio = []
-// document.getElementById('btnAvancar2').addEventListener("click", (evento) => {
-//   evento.preventDefault();
+let relatorio = []
+document.getElementById('btnAvancar2').addEventListener("click", (evento) => {
+  evento.preventDefault();
 
-//   if (!confir.checked) {
-//     alert("Você deve confirmar antes de prosseguir.");
-//   } else if (images.length === 0) {
-//     alert("Você deve inserir pelo menos uma imagem para prosseguir.");
-//   } else {
-//     const confirmacaoEnvio = confirm("Tem certeza de que deseja enviar o relatório?");
+  if (!confir.checked) {
+    alert("VOCÊ DEVE CONFIRMAR ANTES DE PROSSEGUIR.");
+  } else if (images.length === 0) {
+    alert("VOCÊ DEVE INSERIR PELO MENOS UMA IMAGEM PARA PROSSEGUIR.");
+  } else {
+    const confirmacaoEnvio = confirm("TEM CERTEZA QUE DESEJA ENVIAR RELATÓRIO?");
 
-//     if (confirmacaoEnvio) {
-//       if (confir.id == "proteger6") {
-//         let resultado = JSON.parse(localStorage.getItem("relatorio"))
-//         let dadosTemp = JSON.parse(localStorage.getItem("dadosTemp"))
-//         if (resultado == null) {
-//           relatorio.push(dadosTemp)
-//         }
-//         else {
-//           relatorio = resultado
-//           relatorio.push(dadosTemp)
-//         }
-//         localStorage.setItem("relatorio", JSON.stringify(relatorio))
-//         localStorage.removeItem("dadosTemp")
-//         alert("Registro enviado com sucesso!");
-//         window.location.href = "../index.html"
+    if (confirmacaoEnvio) {
+      if (confir.id == "proteger6") {
+        let resultado = JSON.parse(localStorage.getItem("relatorio"))
+        let dadosTemp = JSON.parse(localStorage.getItem("dadosTemp"))
+        if (resultado == null) {
+          relatorio.push(dadosTemp)
+        }
+        else {
+          relatorio = resultado
+          relatorio.push(dadosTemp)
+        }
+        localStorage.setItem("relatorio", JSON.stringify(relatorio))
+        localStorage.removeItem("dadosTemp")
+        alert("REGISTRO ENVIADO COM SUCESSO!");
+        window.location.href = "../index.html"
 
-//       }
-//       window.location.href = '../index.html';
-//     }
-//   }
-
-
-
-// });
+      }
+      window.location.href = '../index.html';
+    }
+  }
+});
 
 
 
